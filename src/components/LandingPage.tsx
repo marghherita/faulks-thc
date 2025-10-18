2e75ca51740f501f411f2e606615af7bb3afe61c
marghherita <margherita.grasso3@gmail.com>
2025-10-18 13:32:35 +0200

    Add THC logo and background image to LandingPage; fix font-family casing in index.css

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,12 +4,25 @@ import { Button } from './ui/button'
 function LandingPage() {
  return (
     <div
-      className='relative bg-[url("/src/assets/images/landing-page-bg.png")] bg-cover bg-no-repeat bg-center w-screen flex items-end justify-center pb-16  h-dvh'
+      className='relative bg-[url("/src/assets/images/sfondo_landing.png")] bg-cover bg-no-repeat bg-center w-screen flex flex-col items-center justify-between pb-16  h-dvh'
     >
+
+      <div className=' h-[50%] pt-12 items-center flex flex-col justify-between '>
+        <div className='items-center flex flex-col just'>
+
+        <img  src="/src/assets/images/thc-logo.png" alt="" />
+        <p className='text-[#FEF3D9] font-[BrosOskon90] font-light text-[1.8rem]'>presentano</p>
+        </div>
+
+        <div className='items-center w-[80%]'>
+          <img  src="/src/assets/images/faulks-landing.png" alt="" />
+        </div>
+      </div>
+
+      
       <Button
         variant="start"
-        size={"start"}
-      >
+        size={"start"}      >
         INIZIA
       </Button>
     </div>
