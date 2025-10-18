bb01cfafcd1b6e71a5230ec36d718064010ed0c9
marghherita <margherita.grasso3@gmail.com>
2025-10-18 13:43:34 +0200

    Update button padding for 'start' variant and clean up LandingPage formatting

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,36 +1,35 @@
-
-import { Button } from './ui/button'
-import ThcLogo from '/src/assets/images/thc-logo.png'
-import FaulksLanding from '/src/assets/images/faulks-landing.png'
+import { Button } from "./ui/button";
+import ThcLogo from "/src/assets/images/thc-logo.png";
+import FaulksLanding from "/src/assets/images/faulks-landing.png";
 
 function LandingPage() {
- return (
-    <div
-      className='relative bg-[url("/src/assets/images/sfondo_landing.png")] bg-cover bg-no-repeat bg-center w-screen flex flex-col items-center justify-between pb-16  h-dvh'
-    >
-
-      <div className=' h-[50%] pt-12 items-center flex flex-col justify-between '>
-        <div className='items-center flex flex-col justify-center'>
-
-        <img  src={ThcLogo} alt="" />
-        <p className='text-[#FEF3D9] font-[BrosOskon90] font-light text-[1.8rem]'>presentano</p>
+  return (
+    <div className='relative bg-[url("/src/assets/images/sfondo_landing.png")] bg-cover bg-no-repeat bg-center w-screen flex flex-col items-center justify-between pb-16  h-dvh'>
+      <div className=" h-[50%] pt-12 items-center flex flex-col justify-between ">
+        <div className="items-center flex flex-col justify-center">
+          <img src={ThcLogo} alt="" />
+          <p className="text-[#FEF3D9] font-[BrosOskon90] font-light text-[1.8rem]">
+            presentano
+          </p>
         </div>
 
-        <div className='items-center w-[80%]'>
-          <img  src={FaulksLanding} alt="" />
+        <div className="items-center w-[80%]">
+          <img src={FaulksLanding} alt="" />
         </div>
       </div>
 
-      
       <Button
         variant="start"
-        size={"start"}      >
+        className=" supports-[backdrop-filter:blur(0px)]:bg-white/10
+        supports-[backdrop-filter:blur(0px)]:backdrop-blur-md"
+        size="start"
+      >
         INIZIA
       </Button>
     </div>
-  )
+  );
 }
 
 // [url("/src/assets/images/landing-page-bg.png")]
 
-export default LandingPage
+export default LandingPage;
