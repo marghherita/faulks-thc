b5b21381abebd2fed2b57c74ea32ad85a4d1878f
marghherita <margherita.grasso3@gmail.com>
2025-10-18 13:01:47 +0200

    Enable start button on LandingPage
    Uncommented the Button component in LandingPage.tsx to display the 'INIZIA' button. This makes the start button visible and interactive on the landing page.

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,18 +1,17 @@
 
-// import { Button } from './ui/button'
+import { Button } from './ui/button'
 
 function LandingPage() {
  return (
     <div
       className='relative bg-blue-500 bg-cover bg-no-repeat bg-center w-screen flex items-end justify-center pb-16 border-3 border-red-500 h-screen'
     >
-      {/* <Button
+      <Button
         variant="start"
         size={"start"}
-        className=" l"
       >
         INIZIA
-      </Button> */}
+      </Button>
     </div>
   )
 }
