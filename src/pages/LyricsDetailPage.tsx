ef9b6093179d460ffd47a099bb7f0846ebca7eee
marghherita <margherita.grasso3@gmail.com>
2025-11-09 12:37:57 +0100

    🗑️ Remove unused SVG imports from LyricsDetailPage component to streamline code

--- src/pages/LyricsDetailPage.tsx
+++ src/pages/LyricsDetailPage.tsx
@@ -8,32 +8,12 @@ import { useState } from "react";
 
 // Import all SVG components
 import FromDusk from "/src/assets/images/from-dusk.svg?react";
-import Atto2 from "/src/assets/images/atto2.svg?react";
-import Atto3 from "/src/assets/images/atto3.svg?react";
-import Atto4 from "/src/assets/images/atto4.svg?react";
-import Atto5 from "/src/assets/images/atto5.svg?react";
-import Atto6 from "/src/assets/images/atto6.svg?react";
-import Atto7 from "/src/assets/images/atto7.svg?react";
-import Atto8 from "/src/assets/images/atto8.svg?react";
-import Atto9 from "/src/assets/images/atto9.svg?react";
-import Atto10 from "/src/assets/images/atto10.svg?react";
-import Atto11 from "/src/assets/images/atto11.svg?react";
-import Atto12 from "/src/assets/images/atto12.svg?react";
+
 
 // Mapping from pathSvg string to React component
 const svgComponents: Record<string, React.ComponentType<React.SVGProps<SVGSVGElement>>> = {
     "/src/assets/images/from-dusk.svg?react": FromDusk,
-    "/src/assets/images/atto2.svg?react": Atto2,
-    "/src/assets/images/atto3.svg?react": Atto3,
-    "/src/assets/images/atto4.svg?react": Atto4,
-    "/src/assets/images/atto5.svg?react": Atto5,
-    "/src/assets/images/atto6.svg?react": Atto6,
-    "/src/assets/images/atto7.svg?react": Atto7,
-    "/src/assets/images/atto8.svg?react": Atto8,
-    "/src/assets/images/atto9.svg?react": Atto9,
-    "/src/assets/images/atto10.svg?react": Atto10,
-    "/src/assets/images/atto11.svg?react": Atto11,
-    "/src/assets/images/atto12.svg?react": Atto12,
+
 };
 
 function LyricsDetailPage({ slug }: { slug: string }) {
